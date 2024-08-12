68ed4bb931c03c2e8d7e6143b580b27a67948a9e
chancelle <mikinhouessechancelle@gmail.com>
2024-08-12 05:16:50 +0100

    Mon message

---
+++ dist/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,js}', // Ajuste le chemin en fonction de la structure de ton projet
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
